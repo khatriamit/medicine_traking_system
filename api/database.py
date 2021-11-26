d874e43c1d4931f606ccaa27f58f1794fc49c475
amiteatziffy <amit.khatri@eatziffy.com>
2021-11-26 14:34:29 +0545

    create database setting/connections

---
+++ api/database.py
@@ -0,0 +1,15 @@
+import sqlalchemy as _sql
+import sqlalchemy.ext.declarative as _declarative
+import sqlalchemy.orm as _orm
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost/medicine"
+
+engine = _sql.create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_size=3,
+)
+
+SessionLocal = _orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = _declarative.declarative_base()
